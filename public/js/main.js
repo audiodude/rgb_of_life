b7f69f1199e140e5b2fab23d5c059946051a2ab1
Sagar Mittal <sagar@sagarmittal.com>
2014-04-30 11:34:58 -0700

    Tweak CSS, use relative paths

--- public/js/main.js
+++ public/js/main.js
@@ -63,7 +63,7 @@ function update_colors() {
     for (var col = 0; col < COLS; col++) {
       color = ~STATE_CUR[cell_index(col, row)] & 0xffffff;
       ctx.fillStyle = '#' + ('000000' + color.toString(16)).substr(-6);
-      ctx.fillRect(col * boxsize, row * boxsize, boxsize, boxsize);
+      ctx.fillRect(col * boxsize + .5, row * boxsize + .5, boxsize, boxsize);
     }
   }
 }
@@ -143,4 +143,6 @@ function playpause() {
 
 $(function() {
   generate_board();
+  preset(3);
+  playpause();
 });
